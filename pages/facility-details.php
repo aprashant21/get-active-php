04198bd248d169e7e62f4e4f634e3d0703b52842
aprashant21 <aprashant21@tbc.edu.np>
2024-05-30 20:58:10 +0545

    feat: show participant details inside facility page

--- pages/facility-details.php
+++ pages/facility-details.php
@@ -50,33 +50,32 @@
 
 <?php
 
-
 if(isset($_GET['id'])) {
     $facility_id = $_GET['id'];
 
-    $sql = "SELECT * FROM facility WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $facility_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    // Fetch facility details
+    $sql_facility = "SELECT * FROM facility WHERE id = ?";
+    $stmt_facility = $conn->prepare($sql_facility);
+    $stmt_facility->bind_param("i", $facility_id);
+    $stmt_facility->execute();
+    $result_facility = $stmt_facility->get_result();
 
-    // Check if the user has already booked the facility
-    $user_id = $_SESSION['user_id'];
-    $sql_check_booking = "SELECT * FROM bookings WHERE user_id = ? AND facility_id = ?";
-    $stmt_check_booking = $conn->prepare($sql_check_booking);
-    $stmt_check_booking->bind_param("ii", $user_id, $facility_id);
-    $stmt_check_booking->execute();
-    $result_check_booking = $stmt_check_booking->get_result();
-
-    if($result->num_rows > 0) {
-        $facility = $result->fetch_assoc();
+    if($result_facility->num_rows > 0) {
+        $facility = $result_facility->fetch_assoc();
     } else {
         $_SESSION['error_message'] = "Facility not found.";
-        header("Location: facilities_list.php");
+        header("Location: facilities.php");
         exit();
     }
+    $stmt_facility->close();
+    $sql_participants = "SELECT u.email, u.username FROM bookings b INNER JOIN users u ON b.user_id = u.id WHERE b.facility_id = ?";
+    $stmt_participants = $conn->prepare($sql_participants);
+    $stmt_participants->bind_param("i", $facility_id);
+    $stmt_participants->execute();
+    $result_participants = $stmt_participants->get_result();
+
 } else {
-    header("Location: facilities_list.php");
+    header("Location: facilities.php");
     exit();
 }
 ?>
@@ -95,13 +94,17 @@ if(isset($_GET['id'])) {
         <h2>Date & Time</h2>
         <p><?php echo date('F j, Y, g:i a', strtotime($facility['date_time'])); ?></p>
         <h2>Participants</h2>
-        <p><?php echo $facility['participants']; ?></p>
-        <?php if ($result_check_booking->num_rows > 0): ?>
-            <h2 style="color:red; font-weight: bold;">Booked</h2>
+        <?php if ($result_participants->num_rows > 0): ?>
+            <ul style="color:black;">
+                <?php while($participant = $result_participants->fetch_assoc()): ?>
+                    <li><?php echo $participant['username']; ?> - <?php echo $participant['email']; ?></li>
+                <?php endwhile; ?>
+            </ul>
         <?php else: ?>
-            <a href="../controllers/bookingController.php?id=<?php echo $facility['id']; ?>" class="book-btn">Book Facility</a>
+            <p>No participants booked for this facility yet.</p>
         <?php endif; ?>
     </div>
 </div>
+
 
 <?php include "../includes/footer.php"; ?>
